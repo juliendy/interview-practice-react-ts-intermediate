6926d87eaf70c8fae0802465773ca24c0f7a328c
juliendy <julien.yasar@gmail.com>
2022-11-15 16:35:53 +0100

    feat: redo button added for extra challange. next refactor

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ type TPoint = {
 
 function App() {
     const [points, setPoints] = useState<TPoint[]>([]);
+    const [popped, setPopped] = useState<TPoint[]>([]);
 
     function handlePlaceCirlce(e: React.MouseEvent<HTMLDivElement>) {
         console.log(e);
@@ -22,22 +23,40 @@ function App() {
     }
 
     function handleUndo() {
-      //TODO: remove the last point added to the array
+        //TODO: remove the last point added to the array
+        const newPoints = [...points];
+        const poppedPoint = newPoints.pop();
+        if (!poppedPoint) return;
+        setPopped([...popped, poppedPoint]);
+        setPoints(newPoints);
+    }
+
+    function handleRedo() {
+        const newPopped = [...popped];
+        const newPoints = [...points];
+        const poppedPoint = newPopped.pop();
+        if (!poppedPoint) return;
+        newPoints.push(poppedPoint);
+        setPoints(newPoints);
+        setPopped(newPopped);
     }
 
     return (
-        <div className="App" onClick={handlePlaceCirlce}>
-          <button onClick={handleUndo}>Undo</button>
-            {points.map((point) => (
-                <div
-                    className="point"
-                    style={{
-                        left: point.x - 5 + "px",
-                        top: point.y - 5 + "px",
-                    }}
-                ></div>
-            ))}
-        </div>
+        <>
+            <button onClick={handleUndo}>Undo</button>
+            <button onClick={handleRedo}>Redo</button>
+            <div className="App" onClick={handlePlaceCirlce}>
+                {points.map((point) => (
+                    <div
+                        className="point"
+                        style={{
+                            left: point.x - 5 + "px",
+                            top: point.y - 5 + "px",
+                        }}
+                    ></div>
+                ))}
+            </div>
+        </>
     );
 }
 
